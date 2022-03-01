0f20367a39924ac159e91cc61068fd7971cb8217
toschilt <lt.toschi@gmail.com>
2022-02-28 23:59:40 -0300

    Webcam interface done!

--- main.cpp
+++ main.cpp
@@ -6,7 +6,35 @@
 
 int main(int argc, char *argv[])
 {
-    
+    cv::VideoCapture webcam(0, cv::CAP_V4L2);
+    cv::namedWindow("originalVideo", cv::WINDOW_AUTOSIZE);
+
+    double fps;
+    fps = webcam.get(cv::CAP_PROP_FPS);
+
+    if(!webcam.isOpened())
+    {
+        std::cout << "Erro em abrir o vídeo!" << std::endl;
+        return -1;
+    }
+
+    while(true)
+    {
+        cv::Mat frame;
+        webcam.read(frame);
+
+        std::cout << "FPS = " << fps << std::endl;
+
+        cv::imshow("originalVideo", frame);
+
+        if(cv::waitKey(10) == 'q')
+        {
+            std::cout << "Vídeo finalizado!" << std::endl;
+            break;
+        }
+    }
+
+    cv::destroyAllWindows();
     
     return 0;
 }
