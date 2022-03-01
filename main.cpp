b0a31856fa63291f3d87759cc4026e8094188e6e
toschilt <lt.toschi@gmail.com>
2022-02-28 23:54:21 -0300

    Basic structure

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+int main(int argc, char *argv[])
+{
+    
+    
+    return 0;
+}
